440738d08542388696da55d95605d919e115ec78
Ankita7520 <ankitaacharjya1999@gmail.com>
2023-05-09 23:04:43 +0530

    secound

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by utsav
+          Learn React by utsav jha
         </a>
       </header>
     </div>
